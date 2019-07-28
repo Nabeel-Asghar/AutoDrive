2deb80246a87f52c4b8bb4ea233140edbe3896ec
Nabeel Asghar <nabeel.asghar156@gmail.com>
2019-07-27 21:27:07 -0400

    Capture screen and have file for direct input

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import numpy as np
+import time
 from PIL import ImageGrab
 import cv2
+from IPython.display import Image
+import pyautogui
 
-def screen_record():
-    while True:
-        printscreen_pil =  ImageGrab.grab()
-        printscreen_numpy =   np.array(printscreen_pil.getdata(),dtype='uint8')\
-        .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
-        cv2.imshow('window',printscreen_numpy)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+for i in range(5,0,-1):
+    print(i)
+    time.sleep(1)
+
+print('down')
+pyautogui.keyDown('w')
+time.sleep(3)
+print('up')
+pyautogui.keyUp('w')
+
+# def image_process(image):
+#
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     grayEdged =  cv2.Canny(gray, threshold1 = 200, threshold2=300)
+#     return(grayEdged)
+#
+#
+# def screen_record():
+#     last_time = time.time()
+#     while True:
+#
+#         for i in list(range(4))[::-1]:
+#     print(i+1)
+#     time.sleep(1)
+#         printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,600)))
+#         new_screen = image_process(printscreen)
+#         cv2.imwrite('messigray.png', new_screen)
+#         num_seconds = time.time() - last_time
+#         print(num_seconds)
+#         if num_seconds > 5:
+#             break
+#         # if cv2.waitKey(25) & 0xFF == ord('q'):
+#         #     cv2.destroyAllWindows()
+#         #     break
+#
+# screen_record()
