566dad8ff5244ec83e8749e9bd6f7afb8ea02d71
Nabeel Asghar <nabeel.asghar156@gmail.com>
2019-07-29 14:23:07 -0400

    Added ROI function

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ from IPython.display import Image
 import pyautogui
 from directkeys import PressKey, W, A, S, D
 
-
+def roi(img, vertices):
+    #blank mask:
+    mask = np.zeros_like(img)
+    # fill the mask
+    cv2.fillPoly(mask, vertices, 255)
+    # now only show the area that is the mask
+    masked = cv2.bitwise_and(img, mask)
+    return(masked)
 
 def image_process(image):
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    grayEdged =  cv2.Canny(gray, threshold1 = 200, threshold2=300)
-    return(grayEdged)
+    processedImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    processedImage =  cv2.Canny(processedImage, threshold1 = 200, threshold2=300)
+    vertices = np.array([[150,700],[150,450],[400,225],[800,225],[850,450],[850,700],], np.int32)
+    processedImage = roi(processedImage, [vertices])
+    return(processedImage)
 
 
 def screen_record():
@@ -22,7 +31,7 @@ def screen_record():
         PressKey(W)
         printscreen =  np.array(ImageGrab.grab(bbox=(0,40,1024,768)))
         new_screen = image_process(printscreen)
-        cv2.imwrite('messigray.png', new_screen)
+        cv2.imwrite('processedImage.png', new_screen)
         num_seconds = time.time() - last_time
         print(num_seconds)
         if num_seconds > 5:
